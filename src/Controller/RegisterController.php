58d9ba9f24fb9faf8146cfa5232d79c5ad195765
elHombreDeLaNoche <elbrandonpro@yahoo.Fr>
2022-05-17 01:42:15 +0200

    BBCv1.2.1

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,17 +2,27 @@
 
 namespace App\Controller;
 
+use App\Form\RegisterType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RegisterController extends AbstractController
 {
     #[Route('/register', name: 'register')]
-    public function index(): Response
+    public function register(Request $request): Response
     {
+        $Register = $this->createForm(RegisterType::class);
+
+        $Register->handleRequest($request);
+
+        if($Register->isSubmitted() && $Register->isValid()){
+            dump($Register->getData());
+        }
+        
         return $this->render('view/_register.html.twig', [
-            'controller_name' => 'RegisterController',
+            'inscription' => $Register->createView()
         ]);
     }
 }
